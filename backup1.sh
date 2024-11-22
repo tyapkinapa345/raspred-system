f0c107c3f33de28ce7a4ccf4072ca7e472aeb2e1
tyapkinapa345 <tyapkina.pol@gmail.com>
2024-11-22 16:27:03 +0300

    Create backup1.sh

---
+++ backup1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Директория с данными и директория для резервных копий
+DATA_DIR=~/data_backup_system/data
+BACKUP_DIR=~/data_backup_system/backup
+
+# Создаем директорию для резервных копий, если она не существует
+mkdir -p $BACKUP_DIR
+
+# Создаем метку времени
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+
+# Копируем данные в резервную директорию с меткой времени
+cp -r $DATA_DIR $BACKUP_DIR/data_$TIMESTAMP
+
+echo "Резервная копия создана: data_$TIMESTAMP"
