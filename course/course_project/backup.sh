b23fa4adae194e39e71ee01adfba8603f6cb2c89
tyapkinapa345 <tyapkina.pol@gmail.com>
2024-11-23 17:06:38 +0300

    Update backup.sh

--- course/course_project/backup.sh
+++ course/course_project/backup.sh
@@ -1,16 +1,18 @@
-   #!/bin/bash
-   exec > /home/devops/course_project/logfile.log 2>&1
-   echo "Резервное копирование началось в $(date)"
-   # Директории
-   DATA_DIR="/home/devops/course_project/data"
-   BACKUP_DIR="/home/devops/course_project/backups"
+#!/bin/bash
+exec > /home/devops/course_project/logfile.log 2>&1
+echo "Резервное копирование началось в $(date)"
+# Директории
+DATA_DIR="/home/devops/course_project/data"
+BACKUP_DIR="/home/devops/course_project/backup"
 
-   # Дата и время
-   TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-   BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
+# Дата и время
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
 
-   mkdir -p "$BACKUP_FILE"
-   # Создание резервной копии
-   tar -czf "$BACKUP_FILE" -C "$DATA_DIR" .
+# Создание директории для резервных копий, если она не существует
+mkdir -p "$BACKUP_DIR"
 
-   echo "Резервная копия создана: $BACKUP_FILE"
+# Создание резервной копии
+tar -czf "$BACKUP_FILE" -C "$DATA_DIR" .
+
+echo "Резервная копия создана: $BACKUP_FILE"
