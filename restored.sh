65826e11bbc99c0fb243ecb830f74c8655480110
tyapkinapa345 <tyapkina.pol@gmail.com>
2024-11-23 11:12:40 +0300

    Create restored.sh

---
+++ restored.sh
@@ -0,0 +1,17 @@
+   #!/bin/bash
+
+   BACKUP_DIR="./backups"
+
+   # Последняя резервная копия
+   LAST_BACKUP=$(ls -t $BACKUP_DIR | head -n 1)
+
+   if [ -z "$LAST_BACKUP" ]; then
+       echo "Нет резервных копий для восстановления."
+       exit 1
+   fi
+
+   # Восстановление
+   TAR_FILE="$BACKUP_DIR/$LAST_BACKUP"
+   tar -xzf "$TAR_FILE" -C ./data
+
+   echo "Восстановление данных из резервной копии: $LAST_BACKUP"
