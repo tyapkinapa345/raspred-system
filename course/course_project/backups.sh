4bff5ecd45d588908a38004fa3d2e45a5caad4df
tyapkinapa345 <tyapkina.pol@gmail.com>
2024-11-23 14:30:11 +0300

    Update and rename backups.sh to course/course_project/backups.sh

--- course/course_project/backups.sh
+++ course/course_project/backups.sh
@@ -1,13 +1,15 @@
    #!/bin/bash
-
+   exec > /home/devops/course_project/logfile.log 2>&1
+   echo "Резервное копирование началось в $(date)"
    # Директории
-   DATA_DIR="./data"
-   BACKUP_DIR="./backups"
+   DATA_DIR="/home/devops/course_project/data"
+   BACKUP_DIR="/home/devops/course_project/backups"
 
    # Дата и время
    TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
    BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
 
+   mkdir -p "$BACKUP_FILE"
    # Создание резервной копии
    tar -czf "$BACKUP_FILE" -C "$DATA_DIR" .
 
