5bc87c80756cd857680da37146e325fc7b326c9b
tyapkinapa345 <tyapkina.pol@gmail.com>
2024-11-23 16:56:22 +0300

    Update restore.sh

--- course/course_project/restore.sh
+++ course/course_project/restore.sh
@@ -1,17 +1,22 @@
-   #!/bin/bash
+#!/bin/bash
 
-   BACKUP_DIR="/home/devops/data_recovery_system/backups"
+BACKUP_DIR="/home/devops/course_project/backup"
 
-   # Последняя резервная копия
-   LAST_BACKUP=$(ls -t $BACKUP_DIR | head -n 1)
+# Последняя резервная копия
+LAST_BACKUP=$(ls -t $BACKUP_DIR | grep -v '/$' | head -n 1)
 
-   if [ -z "$LAST_BACKUP" ]; then
-       echo "Нет резервных копий для восстановления."
-       exit 1
-   fi
+if [ -z "$LAST_BACKUP" ]; then
+    echo "Нет резервных копий для восстановления."
+    exit 1
+fi
 
-   # Восстановление
-   TAR_FILE="$BACKUP_DIR/$LAST_BACKUP"
-   tar -xzf "$TAR_FILE" -C ./data
+# Восстановление
+TAR_FILE="$BACKUP_DIR/$LAST_BACKUP"
+if [ ! -f "$TAR_FILE" ]; then
+    echo "Файл резервной копии не найден или не является файлом."
+    exit 1
+fi
 
-   echo "Восстановление данных из резервной копии: $LAST_BACKUP"
+tar -xzf "$TAR_FILE" -C /home/devops/course_project/data
+
+echo "Восстановление данных из резервной копии: $LAST_BACKUP"
