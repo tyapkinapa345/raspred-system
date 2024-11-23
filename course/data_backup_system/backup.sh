2b52b08ca79eabb3f95d254137cab7e4b49c4b42
tyapkinapa345 <tyapkina.pol@gmail.com>
2024-11-23 14:40:51 +0300

    Update and rename backup1.sh to course/data_backup_system/backup.sh

--- course/data_backup_system/backup.sh
+++ course/data_backup_system/backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+exec > /home/devops/data_backup_system/logfile.log 2>&1
+echo "Резервное копирование началось в $(date)"
 # Директория с данными и директория для резервных копий
-DATA_DIR=~/data_backup_system/data
-BACKUP_DIR=~/data_backup_system/backup
+DATA_DIR="/home/devops/data_backup_system/data"
+BACKUP_DIR="/home/devops/data_backup_system/backup"
 
 # Создаем директорию для резервных копий, если она не существует
 mkdir -p $BACKUP_DIR
@@ -11,6 +12,6 @@ mkdir -p $BACKUP_DIR
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 # Копируем данные в резервную директорию с меткой времени
-cp -r $DATA_DIR $BACKUP_DIR/data_$TIMESTAMP
+cp -r "$DATA_DIR" "$BACKUP_DIR/data_$TIMESTAMP"
 
 echo "Резервная копия создана: data_$TIMESTAMP"
